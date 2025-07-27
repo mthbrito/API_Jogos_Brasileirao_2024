1641e57f4e283a41aa098b72012cc67f9dcc7483
Matheus Brito <matheus.sbrito14@gmail.com>
2025-07-27 16:15:30 -0300

    update: Exclusão de comentário

--- API_Jogos_Brasileirao_2024/src/main/java/com/example/API_Jogos_Brasileirao_2024/controller/TimeController.java
+++ API_Jogos_Brasileirao_2024/src/main/java/com/example/API_Jogos_Brasileirao_2024/controller/TimeController.java
@@ -7,14 +7,12 @@ import com.example.API_Jogos_Brasileirao_2024.service.TimeService;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("api/brasileirao2024")
 public class TimeController {
